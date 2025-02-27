84bfc5835588e60d9a3131cf1f13edd758f85a7c
venket-abhishek <125969100+venket-abhishek@users.noreply.github.com>
2025-02-27 12:22:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var swiper = new Swiper(".swiper-container", {
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        zoom: true
+    });
+});
